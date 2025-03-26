ee945300a829e1640247391fd2b26b9875470cdc
Stelian Hristov <mtel19999@gmail.com>
2025-03-26 23:11:12 +0200

    fixing little mistake

--- HomeInventory/HomeForm.Designer.cs
+++ HomeInventory/HomeForm.Designer.cs
@@ -192,7 +192,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 30);
             this.button1.TabIndex = 12;
-            this.button1.Text = "Export to Exel";
+            this.button1.Text = "Export to Excel";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
